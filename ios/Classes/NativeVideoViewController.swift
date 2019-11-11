e5070c720f77d3ec9ae9f133f084feaa377d72b3
Luis Andres Jara Castillo <luis.jara.castillo@ceisufro.cl>
2019-11-11 17:06:19 -0300

    Completed iOS code for native player.

--- ios/Classes/NativeVideoViewController.swift
+++ ios/Classes/NativeVideoViewController.swift
@@ -11,36 +11,29 @@ import Flutter
 import AVFoundation
 
 public class NativeVideoViewController: NSObject, FlutterPlatformView {
-    private let viewId: Int64
-    private let methodChannel: FlutterMethodChannel
-    private let videoView: UIView
-    private let videoPlayer: AVPlayer
-    private let videoPlayerLayer: AVPlayerLayer
-    private var initialized: Bool = false
-    private var videoAsset: AVAsset? = nil
+    private var viewId: Int64
+    private var methodChannel: FlutterMethodChannel
+    private var videoView: VideoView?
     
     init(frame:CGRect, viewId:Int64, registrar: FlutterPluginRegistrar) {
         self.viewId = viewId
-        self.videoView = UIView(frame: frame)
-        self.videoPlayer = AVPlayer(playerItem: nil)
-        self.videoPlayerLayer = AVPlayerLayer(player: self.videoPlayer)
-        self.videoView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-        self.videoView.layer.addSublayer(videoPlayerLayer)
+        self.videoView = VideoView(frame: frame)
         self.methodChannel = FlutterMethodChannel(name: "native_video_view_\(viewId)", binaryMessenger: registrar.messenger())
         super.init()
+        self.videoView?.addOnPreparedObserver(callback: self.onPrepared)
+        self.videoView?.addOnFailedObserver(callback: self.onFailed(message:))
+        self.videoView?.addOnCompletionObserver(callback: self.onCompletion)
         self.methodChannel.setMethodCallHandler(handle)
     }
     
     deinit {
-        self.initialized = false
+        self.videoView = nil
         self.methodChannel.setMethodCallHandler(nil)
-        self.videoPlayer.removeObserver(self, forKeyPath: "status")
         NotificationCenter.default.removeObserver(self)
-        self.stopPlayback()
     }
     
     public func view() -> UIView {
-        return videoView
+        return videoView!
     }
     
     func handle(call: FlutterMethodCall, result: FlutterResult) -> Void {
@@ -52,40 +45,38 @@ public class NativeVideoViewController: NSObject, FlutterPlatformView {
                 let sourceType: String? = args["sourceType"] as? String
                 if let path = videoPath{
                     let isUrl: Bool = sourceType == "VideoSourceType.network" ? true : false
-                    initVideo(videoPath: path, isURL: isUrl)
+                    self.videoView?.configure(videoPath: path, isURL: isUrl)
                 }
             }
             result(nil)
             break
         case "player#start":
-            self.startPlayback()
+            self.videoView?.play()
             result(nil)
             break
         case "player#pause":
-            self.pausePlayback(restart: false)
+            self.videoView?.pause(restart: false)
             result(nil)
             break
         case "player#stop":
-            self.stopPlayback()
+            self.videoView?.stop()
             result(nil)
             break
         case "player#currentPosition":
             var arguments = Dictionary<String, Any>()
-            arguments["currentPosition"] = self.getCurrentPosition()
+            arguments["currentPosition"] = self.videoView?.getCurrentPosition()
             result(arguments)
             break
         case "player#isPlaying":
             var arguments = Dictionary<String, Any>()
-            arguments["isPlaying"] = self.isPlaying()
+            arguments["isPlaying"] = self.videoView?.isPlaying()
             result(arguments)
             break
         case "player#seekTo":
             let arguments = call.arguments as? [String:Any]
             if let args = arguments {
                 let position: Int64? = args["position"] as? Int64
-                if let pos = position {
-                    self.videoPlayer.seek(to: CMTimeMake(pos, 1000), toleranceBefore: kCMTimeZero, toleranceAfter: kCMTimeZero)
-                }
+                self.videoView?.seekTo(positionInMillis: position)
             }
             result(nil)
             break
@@ -95,130 +86,23 @@ public class NativeVideoViewController: NSObject, FlutterPlatformView {
         }
     }
     
-    func initVideoPlayer(){
-        self.videoPlayer.addObserver(self, forKeyPath: "status", options: [], context: nil)
-        self.initialized = true
-    }
-    
-    func updateVideoLayer(width: Double, height:Double) {
-        let videoHeight = self.getVideoWidth()
-        let videoWidth = self.getVideoWidth()
-        let orientation = UIApplication.shared.statusBarOrientation
-        let screenRect = UIScreen.main.bounds
-        var height = 0.0
-        var width = 0.0
-        self.videoPlayerLayer.frame = self.videoView.bounds
-    }
-    
-    func initVideo(videoPath:String?, isURL: Bool){
-        if !initialized {
-            self.initVideoPlayer()
-        }
-        if let path = videoPath {
-            let uri: URL? = isURL ? URL(string: path) : URL(fileURLWithPath: path)
-            let asset = AVAsset(url: uri!)
-            self.videoPlayer.replaceCurrentItem(with: AVPlayerItem(asset: asset))
-            self.videoAsset = asset
-            // Notifies when the video finishes playing.
-            NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying(notification:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.videoPlayer.currentItem)
-        }
-    }
-    
-    func startPlayback(){
-        if !self.isPlaying() && self.videoAsset != nil {
-            self.videoPlayer.play()
-        }
-    }
-    
-    func pausePlayback(restart:Bool){
-        self.videoPlayer.pause()
-        if(restart){
-            self.videoPlayer.seek(to: kCMTimeZero)
-        }
-    }
-    
-    func stopPlayback(){
-        self.pausePlayback(restart: true)
-    }
-    
-    func isPlaying() -> Bool{
-        return self.videoPlayer.rate != 0 && self.videoPlayer.error == nil
-    }
-    
-    func getDuration()-> Int64 {
-        let durationObj = self.videoPlayer.currentItem?.asset.duration
-        return self.transformCMTime(time: durationObj)
-    }
-    
-    func getCurrentPosition() -> Int64 {
-        let currentTime = self.videoPlayer.currentItem?.currentTime()
-        return self.transformCMTime(time: currentTime)
-    }
-    
-    func getVideoHeight() -> Double {
-        var height: Double = 0.0
-        let videoTrack = self.getVideoTrack()
-        if videoTrack != nil {
-            height = Double(videoTrack?.naturalSize.height ?? 0.0)
-        }
-        return height
-    }
-    
-    func getVideoWidth() -> Double {
-        var width: Double = 0.0
-        let videoTrack = self.getVideoTrack()
-        if videoTrack != nil {
-            width = Double(videoTrack?.naturalSize.width ?? 0.0)
-        }
-        return width
-    }
-    
-    func getVideoTrack() -> AVAssetTrack? {
-        var videoTrack: AVAssetTrack? = nil
-        let tracks = videoAsset?.tracks(withMediaType: .video)
-        if tracks != nil && tracks!.count > 0 {
-            videoTrack = tracks![0]
-        }
-        return videoTrack
-    }
-    
-    func transformCMTime(time:CMTime?) -> Int64 {
-        var ts : Double = 0
-        if let obj = time {
-            ts = CMTimeGetSeconds(obj) * 1000
-        }
-        return Int64(ts)
-    }
-    
-    @objc func playerDidFinishPlaying(notification: NSNotification){
+    func onCompletion(){
         self.methodChannel.invokeMethod("player#onCompletion", arguments: nil)
     }
     
-    override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "status" {
-            let status = self.videoPlayer.status
-            switch(status){
-            case .unknown:
-                print("Status unknown")
-            case .readyToPlay:
-                var arguments = Dictionary<String, Any>()
-                arguments["duration"] = self.getDuration()
-                let height = self.getVideoHeight()
-                let width = self.getVideoWidth()
-                self.updateVideoLayer(width: width, height: height)
-                arguments["height"] = height
-                arguments["width"] = width
-                self.methodChannel.invokeMethod("player#onPrepared", arguments: arguments)
-                break
-            case .failed:
-                self.videoAsset = nil
-                var arguments = Dictionary<String, Any>()
-                if let error = self.videoPlayer.error{
-                    arguments["message"] = error.localizedDescription
-                }
-                self.methodChannel.invokeMethod("player#onError", arguments: arguments)
-                break
-            }
-        }
+    func onPrepared(){
+        var arguments = Dictionary<String, Any>()
+        let height = self.videoView?.getVideoHeight()
+        let width = self.videoView?.getVideoWidth()
+        arguments["duration"] = self.videoView?.getDuration()
+        arguments["height"] = height
+        arguments["width"] = width
+        self.methodChannel.invokeMethod("player#onPrepared", arguments: arguments)
+    }
+    
+    func onFailed(message: String){
+        var arguments = Dictionary<String, Any>()
+        arguments["message"] = message
+        self.methodChannel.invokeMethod("player#onError", arguments: arguments)
     }
 }
